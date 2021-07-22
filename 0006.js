f17faf31b63b2b4e7a5c95be79de4d66297de158
unmem <unmem@outlook.com>
2021-07-22 10:50:38 +0800

    experiment

--- 0006.js
+++ 0006.js
@@ -2,8 +2,8 @@
 //
 //   1. change variable name
 
-var a = 10           // change to a1
+var a = 10           // change to "var a1", or do not change at all
 var b = a + 20 * a
 
-a = { value: 1024 }  // change to a2
+a = { value: 1024 }  // change to "var a2"
 var c = a.value + 20 * a.value
